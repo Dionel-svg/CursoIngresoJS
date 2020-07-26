0f2cd4bbff96c01a9c62ab122f1cb2274ce01f5f
Dionel-svg <dio.smart.inc@gmail.com>
2020-07-26 14:59:37 -0300

    8

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,17 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+	let estado;
+
+	edad= document.getElementById("txtIdEdad").value;
+	estado= document.getElementById("estadoCivil").value;
+
+	if(edad >=18 && estado=="Soltero")
+	{
+
+		alert("Es soltero y no es menor ");
+	}
 	
 
 
-}//FIN DE LA FUNCIÓN
+}
