db23362c3a394981201348fa8b1acc48e449572f
Dionel-svg <dio.smart.inc@gmail.com>
2020-07-26 02:27:02 -0300

    6

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,20 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
 
+	edad = parseInt(document.getElementById("txtIdEdad").value);
 
+	if(edad>=13 && edad <=17)
+	
+  {
+	  
+	alert("Es adolescente");
+  }
+if(edad<13){
+	alert("Es niño");
+}
+else{
+	alert("Es mayor de edad");
+}
 
-}//FIN DE LA FUNCIÓN
+}
