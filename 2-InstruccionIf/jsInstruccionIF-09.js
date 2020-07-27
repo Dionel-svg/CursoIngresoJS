a3e87234447d40886193c5021fe3f95f201f59f5
Dionel-svg <dio.smart.inc@gmail.com>
2020-07-26 23:00:30 -0300

    9

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,6 +1,14 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");	
 
-}//FIN DE LA FUNCIÓN
+
+let numero;    
+let maximo=10;
+let minimo =1;
+
+numero = Math.round(Math.random()* (maximo-minimo)+ minimo);
+
+alert(numero);
+
+
+}
