cf0443e25391e988ed40c7cda59f7b67f1c51945
Dionel-svg <dio.smart.inc@gmail.com>
2020-07-27 00:03:36 -0300

    1

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,9 +1,22 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	mes= document.getElementById("txtIdMes").value;
+	
+	switch (mes){
+		case "Enero":
+			alert("Que comiences bien el año!!!.");
+			break;
+			case "Marzo":
+			alert("A clases!!!.");
+			break;
+			case "Julio":
+			alert("Se vienen las vacaciones!!!.");
+			break;
+			case "Diciembre":
+			alert("Felices fiesta!!!.");
+			break;
+			
+	}
 
 
-
-}//FIN DE LA FUNCIÓN
+}
