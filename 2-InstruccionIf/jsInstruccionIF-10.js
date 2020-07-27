cb22cc809ae39a156327de9f2645f2378efe25fe
Dionel-svg <dio.smart.inc@gmail.com>
2020-07-26 23:25:49 -0300

    10

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,21 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	
+let nota;    
+let maximo=10;
+let minimo =1;
 
-}//FIN DE LA FUNCIÓN
+nota = Math.round(Math.random()* (maximo-minimo)+ minimo);
+
+
+if(nota==9 || nota ==10){
+	alert("Excelente Nota: "+ nota);
+}
+else if (nota>=4)
+{
+	alert("Aprobó Nota: "+nota);
+}
+else {
+		alert("Vamos la proxima se puede Nota: "+ nota);
+}
+}
